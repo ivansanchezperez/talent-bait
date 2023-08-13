1b7be3541773df6b2bd8fc9374baafb5e36fa8ea
ivansanchezperez <ivan@DESKTOP-C5UAKBC>
2023-08-13 21:43:15 +0200

    🧪: removed failing test

--- src/create-view/tests/create-view.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import IndexView from "../read-view";
-
-test("renders learn react link", () => {
-  render(<IndexView />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
