2b06a9e513a02098ca1cac4daaa121ae94b99744
ivansanchezperez <ivan@DESKTOP-C5UAKBC>
2023-08-13 10:31:28 +0200

    ✅: update test

--- src/index-view/tests/index-view.test.js
+++ src/index-view/tests/index-view.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "../app";
+import IndexView from "../index-view";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(<IndexView />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
